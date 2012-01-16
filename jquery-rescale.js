f931e5eb78e0ad265ea415e5943e35984a002a8b
talos <irving.krauss@gmail.com>
2012-01-16 13:51:36 -0500

    updating to 0.2.0

--- jquery-rescale.js
+++ jquery-rescale.js
@@ -32,92 +32,98 @@
    implied, of John Krauss.
 **/
 
-/**
-   DOCUMENTATION
+/*globals jQuery*/
 
-   See README.md
-**/
-
-/**
-   VERSIONS
-
-   0.1.0 : Initial release
-**/
-
-/**
-   CODE
-**/
-(function($) {
+(function ($) {
+    "use strict";
     var properties = [
         [ 'transform', 'transform-origin' ],
-        [ '-ms-transform','-ms-transform-origin'], /* IE 9 */
+        [ '-ms-transform', '-ms-transform-origin'], /* IE 9 */
         [ '-webkit-transform', '-webkit-transform-origin'],/* Safari and Chrome */
         [ '-o-transform', '-o-transform-origin'], /* Opera */
         [ '-moz-transform', '-moz-transform-origin' ] /* Firefox */
     ],
 
-    len = properties.length,
+        len = properties.length,
+
+        defaults = {
+            w: 50,
+            h: 50,
+            distort: true,
+            direction: 0
+        },
+
+        /**
+         Construct a JS object hash that can be applied as CSS to do
+         transform and transform-origin value across browsers.
+
+         @param transform the value for the transform CSS property.
+         @param transformOrigin the value for the transform-origin CSS
+         property.
+
+         @return A JS object that can be applied as CSS.
+         **/
+        buildCSS = function (transform, transformOrigin) {
+            var memo = {},
+                i; // this is so much more elegant using reduce. le sigh.
+            //while (++i < len) {
+            for (i = 0; i < len; i += 1) {
+                memo[properties[i][0]] = transform;
+                memo[properties[i][1]] = transformOrigin;
+            }
+            return memo;
+        };
 
     /**
-       Construct a JS object hash that can be applied as CSS to do
-       transform and transform-origin value across browsers.
-
-       @param transform the value for the transform CSS property.
-       @param transformOrigin the value for the transform-origin CSS
-       property.
-
-       @return A JS object that can be applied as CSS.
-    **/
-    buildCSS = function(transform, transformOrigin) {
-        var memo = {},
-        i = -1; // this is so much more elegant using reduce. le sigh.
-        while(++i < len) {
-            memo[properties[i][0]] = transform;
-            memo[properties[i][1]] = transformOrigin;
-        }
-        return memo;
-    };
-
-    /**
-       Scale an element to a specific width and height.
-
-       @param w The width to scale to.  Required.
-       @param h The height to scale to.  Required.
-       @param distort Whether to allow differing x and y scale
-       factors.  Is true by default, meaning the element could be
-       distorted.  If false, the smallest of the two scales will be
-       used for both axes, so that neither w or h is ever exceeded.
-       @param scaleDirection If negative, only scaling down will be
-       allowed.  If greater than 0, only scaling up will be allowed.
-       Otherwise, either direction is allowed.  Defaults to 0.
-    **/
-    $.fn.rescale = function(w, h, distort, scaleDirection) {
-        if(w === null || typeof w === 'undefined'
-           || h === null || typeof h === 'undefined') {
+     * Scale an element to a specific width and height.
+     *
+     * @param w The width to scale to.  Required.
+     * @param h The height to scale to.  Required.
+     * @param options A JS object of options.  Can be ignored.
+     * The following options are read:
+     *       `x`: The x center to scale from, as a percentage from 0
+     * to 100.  Defaults to 50.
+     *       `y`: The y center to scale from, as a percentage from 0
+     * to 100. Defaults to 50.
+     *       `distort`:  Whether to allow differing x and y scale
+     * factors.  Is true by default, meaning the element could be
+     * distorted.  If false, the smallest of the two scales will be
+     * used for both axes, so that neither w or h is ever exceeded.
+     *       `direction` If negative, only scaling down will be
+     * allowed.  If greater than 0, only scaling up will be allowed.
+     * Otherwise, either direction is allowed.  Defaults to 0.
+     *
+     * @return The elements this was called upon, allowing for chaining.
+     **/
+    $.fn.rescale = function (w, h, options) {
+        if (w === null || typeof w === 'undefined' || h === null || typeof h === 'undefined') {
             return this;
         }
-        return $.each(this, function(i, el) {
-            var $el = $(el),
-            xScale = w / $el.width(),
-            yScale = h / $el.height();
+        var settings = $.extend({}, defaults, options);
 
-            if(scaleDirection > 0) { // allow scaling only up
+        return $.each(this, function (i, el) {
+            var $el = $(el),
+                xScale = w / $el.width(),
+                yScale = h / $el.height();
+
+            if (settings.direction > 0) { // allow scaling only up
                 xScale = xScale < 1 ? 1 : xScale;
                 yScale = yScale < 1 ? 1 : yScale;
-            } else if(scaleDirection < 0) { // allow scaling only down
+            } else if (settings.direction < 0) { // allow scaling only down
                 xScale = xScale > 1 ? 1 : xScale;
                 yScale = yScale > 1 ? 1 : yScale;
             }
 
-            if(distort === false) {
-                if(xScale > yScale) {
+            if (settings.distort === false) {
+                if (xScale > yScale) {
                     xScale = yScale;
                 } else {
                     yScale = xScale;
                 }
             }
 
-            $el.css(buildCSS('scale(' + xScale + ',' + yScale + ')', '0 0'));
+            $el.css(buildCSS('scale(' + xScale + ',' + yScale + ')',
+                             settings.x + '% ' + settings.y + '%'));
         });
     };
-})(jQuery);
+}(jQuery));
